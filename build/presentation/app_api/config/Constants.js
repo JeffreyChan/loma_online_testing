189ed56374694304dfc7ee689fa6a84be459e4f4
Jeffrey Chan <cdjboy@163.com>
2016-05-20 09:32:36 +0800

    delete build folder

--- build/presentation/app_api/config/Constants.js
+++
@@ -1,8 +0,0 @@
-var Constants = (function () {
-    function Constants() {
-    }
-    Constants.DB_CONNECTION_STRING = "mongodb://localhost/loma";
-    return Constants;
-})();
-Object.seal(Constants);
-module.exports = Constants;
