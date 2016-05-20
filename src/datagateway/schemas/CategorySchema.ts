962072a238f77baa06dfeb9f732f213e25a2a145
JeffreyChan <cdjboy@163.com>
2016-05-21 00:41:58 +0800

    add mongoose-unique-validator, but not work

--- src/datagateway/schemas/CategorySchema.ts
+++ src/datagateway/schemas/CategorySchema.ts
@@ -1,3 +1,4 @@
+var uniqueValidator = require('mongoose-unique-validator');
 import DataAccess = require("./../DataAccess");
 import ICategory = require("./../../domainmodel/ICategoryModel.ts");
 
@@ -7,13 +8,15 @@ var Schema = mongoose.Schema;
 
 class CategorySchema {
     static get schema() {
-        return new mongoose.Schema({
+        var catSchema =  mongoose.Schema({
             name: { type: String, index: 1, required: true, unique: true },
             desc: { type: String },
             parent: { type: Schema.Types.ObjectId, ref: 'Category' },
             childrens: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
             create_date: { type: Date, default: Date.now },
         }, { collection: 'categories' });
+        catSchema.plugin(uniqueValidator);
+        return catSchema;
     }
 }
 
