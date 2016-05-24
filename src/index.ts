f06090e46cf94c3196f7ac8d64a66260e8df7858
JeffreyChan <cdjboy@163.com>
2016-05-24 22:46:48 +0800

    add index.html

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 /// <reference path="./../typings/tsd.d.ts" />
 
 import express = require("express");
+import path = require("path");
 import Middlewares = require("./app_api/middlewares/MiddlewaresBase");
 
 var app = express();
 var port = parseInt(process.env.PORT, 10) || 3000;
 app.set("port", port);
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(Middlewares.configuration);
 
+app.use((req: express.Request, res: express.Response) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 app.listen(port, () => {
     console.log("Node app is running at localhost:" + port);
 });
